58480d1f46294fd9be3fbfb404b63d20edaae764
Emily Joanne Castillo Martinez <a00839559@tec.mx>
2026-03-24 09:56:29 -0600

    Crear pagina MyProfile

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Fanatic from './pages/Fanatic'
 import Login from './pages/Login'
+import MyProfile from './pages/MyProfile';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />
       <Route path="/fanatic" element={<Fanatic />} />
+      <Route path="/myprofile" element={<MyProfile />} />
     </Routes>
   );
 }
---
+++ React/src/pages/MyProfile.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/layout/NavBar";
+
+function MyProfile() {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+}
+
+export default MyProfile;
