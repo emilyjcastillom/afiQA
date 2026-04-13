5c6e3622a11d15f561400fb0a2e2f440fb325e1f
Alan-CF <alanyaco.cf@gmail.com>
2026-04-12 19:42:17 -0600

    [FIX] Navbar merge

--- React/src/components/layout/NavBar.tsx
+++ React/src/components/layout/NavBar.tsx
@@ -10,39 +10,31 @@ export default function NavBar() {
     const [isCartOpen, setIsCartOpen] = useState(false);
 
     return (
-        <nav className="flex items-center justify-between p-4 bg-secondary text-white">
-            <button onClick={ () => navigate("/")} className="cursor-pointer">
-                <img src="/logo.png" alt="Logo" className="h-10"/>
-            </button>
-            <div className="flex items-center gap-5">
-                <ShoppingBagIcon className="w-6 h-6" aria-hidden="true" />
-                <Bars3Icon className="w-6 h-6" aria-hidden="true" />
-                <button
-                    type="button"
-                    onClick={() => navigate("/myprofile")}
-                >
         <>
             <nav className="flex items-center justify-between p-4 bg-secondary text-white">
                 <button onClick={() => navigate("/")} className="cursor-pointer">
                     <img src="/logo.png" alt="Logo" className="h-10" />
                 </button>
+
                 <div className="flex items-center gap-5">
                     <button onClick={() => setIsCartOpen(true)} type="button" className="cursor-pointer">
                         <ShoppingBagIcon className="w-6 h-6" aria-hidden="true" />
                     </button>
+
                     <Bars3Icon className="w-6 h-6" aria-hidden="true" />
-                    {user?.avatar_url ? (
-                        <img
-                            src={user.avatar_url}
-                            alt="Avatar"
-                            className="w-10 h-10 rounded-full object-cover"
-                        />
-                    ) : (
-                        <UserCircleIcon className="w-10 h-10" aria-hidden="true" />
-                    )}
-                </button>
-            </div>
-        </nav>
+                    <button type="button" onClick={() => navigate("/myprofile")} className="cursor-pointer">
+                        {user?.avatar_url ? (
+                            <img
+                                src={user.avatar_url}
+                                alt="Avatar"
+                                className="w-10 h-10 rounded-full object-cover"
+                            />
+                        ) : (
+                            <UserCircleIcon className="w-10 h-10" aria-hidden="true" />
+                        )}
+                    </button>
+
+                    
                 </div>
             </nav>
             <Cart isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
