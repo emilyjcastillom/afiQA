a10afce5efaa63f2a1c414b4f16a016462680a44
Emily Joanne Castillo Martinez <a00839559@tec.mx>
2026-04-16 17:37:50 -0600

    Update redirectTo URL for GitHub Pages

--- React/src/lib/auth.ts
+++ React/src/lib/auth.ts
@@ -4,7 +4,7 @@ export async function signInWithGoogle() {
   return await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${window.location.origin}/`,
+      redirectTo: `${window.location.origin}/afiQA/`,
     },
   })
 }
